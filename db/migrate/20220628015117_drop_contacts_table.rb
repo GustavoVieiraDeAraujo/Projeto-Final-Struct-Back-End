a2ec945bb3bacd02b9c7a6b5b8419030b205d343
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-06-30 10:38:52 -0300

    Removendo uma migration orfa que tava quebrando o db:migrate do zero

--- db/migrate/20220628015117_drop_contacts_table.rb
+++
@@ -1,9 +0,0 @@
-class DropContactsTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :contacts do |t|
-      t.string :email, null: false
-      t.string :name, null: false
-      t.string :about, null: false
-    end
-  end
-end
